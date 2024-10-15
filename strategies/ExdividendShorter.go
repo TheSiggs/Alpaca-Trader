b9655f5f13ed54eb46636312a2ebab9e1b213b5a
TheSiggs <samsiggs00@gmail.com>
2024-10-16 08:20:19 +1300

    Update take profit

--- strategies/ExdividendShorter.go
+++ strategies/ExdividendShorter.go
@@ -69,7 +69,7 @@ func ExdividendShorter() {
     if quote.AskPrice == 0 {
         currentPrice = quote.BidPrice
     }
-	takeProfitPrice := decimal.NewFromFloat((currentPrice * 0.92))
+	takeProfitPrice := decimal.NewFromFloat((currentPrice * 0.98))
 	stopLossPrice := decimal.NewFromFloat((currentPrice * 1.02))
     log.Printf("Current Price: %v, Take Profit: %v, Stop Loss %v", currentPrice, takeProfitPrice, stopLossPrice)
 
